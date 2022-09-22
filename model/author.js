f41c97286ac8ed40d0dc5a334dd20932baf3f728
mahmoud zaky <78617936+zaky-fetoh@users.noreply.github.com>
2022-09-22 12:08:05 +0200

    the Author Schema is added

--- model/author.js
+++ model/author.js
@@ -4,22 +4,30 @@ const joi = require("joi");
 
 
 const authorSchema = mongoose.Schema({
-    _id:{
+    _id: {
         type: mongoose.Schema.Types.ObjectId,
         default: mongoose.Types.ObjectId,
     },
     first_name: {
-        type: String, 
+        type: String,
         requied: true,
     },
-    last_name:{
-        type: String, 
+    last_name: {
+        type: String,
         required: true,
     }
 });
 
 const joiAuthor = joi.object({
     _id: any(),
-    first_name: joi.string().max(265),
-    last_name: joi.string
+    first_name: joi.string().max(265).min(3).required(),
+    last_name: joi.string().max(256).min(3).required(),
 })
+authorSchema.pre("save", async function (next) {
+    const { error } = joiAuthor.validate(this._doc);
+    if (error) throw error;
+    next()
+})
+
+
+module.exports = mongoose.model("auhor", authorSchema)
